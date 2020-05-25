70204583be4c4453d282357257274e49e43a3b3c
Mrmoore98 <moooooore66@gmail.com>
2020-05-25 20:42:36 +0800

    aaa

--- models/oh_cnn_HAN/sen_word_field.py
+++ models/oh_cnn_HAN/sen_word_field.py
@@ -5,84 +5,31 @@ import torch
 from tqdm import tqdm
 
 from torchtext.data.dataset import Dataset
-from torchtext.data.pipeline import Pipeline
-from torchtext.data.utils import get_tokenizer, dtype_to_attr, is_tokenizer_serializable
-from torchtext.vocab import Vocab, SubwordVocab
-
-
+from torchtext.data import Field
 
 class SentenceWord_field(Field):
-    """A nested field.
-
-    A nested field holds another field (called *nesting field*), accepts an untokenized
-    string or a list string tokens and groups and treats them as one field as described
-    by the nesting field. Every token will be preprocessed, padded, etc. in the manner
-    specified by the nesting field. Note that this means a nested field always has
-    ``sequential=True``. The two fields' vocabularies will be shared. Their
-    numericalization results will be stacked into a single tensor. And NestedField will
-    share the same include_lengths with nesting_field, so one shouldn't specify the
-    include_lengths in the nesting_field. This field is
-    primarily used to implement character embeddings. See ``tests/data/test_field.py``
-    for examples on how to use this field.
-
-    Arguments:
-        nesting_field (Field): A field contained in this nested field.
-        use_vocab (bool): Whether to use a Vocab object. If False, the data in this
-            field should already be numerical. Default: ``True``.
-        init_token (str): A token that will be prepended to every example using this
-            field, or None for no initial token. Default: ``None``.
-        eos_token (str): A token that will be appended to every example using this
-            field, or None for no end-of-sentence token. Default: ``None``.
-        fix_length (int): A fixed length that all examples using this field will be
-            padded to, or ``None`` for flexible sequence lengths. Default: ``None``.
-        dtype: The torch.dtype class that represents a batch of examples
-            of this kind of data. Default: ``torch.long``.
-        preprocessing (Pipeline): The Pipeline that will be applied to examples
-            using this field after tokenizing but before numericalizing. Many
-            Datasets replace this attribute with a custom preprocessor.
-            Default: ``None``.
-        postprocessing (Pipeline): A Pipeline that will be applied to examples using
-            this field after numericalizing but before the numbers are turned
-            into a Tensor. The pipeline function takes the batch as a list, and
-            the field's Vocab. Default: ``None``.
-        include_lengths: Whether to return a tuple of a padded minibatch and
-            a list containing the lengths of each examples, or just a padded
-            minibatch. Default: False.
-        tokenize: The function used to tokenize strings using this field into
-            sequential examples. If "spacy", the SpaCy tokenizer is
-            used. If a non-serializable function is passed as an argument,
-            the field will not be able to be serialized. Default: string.split.
-        tokenizer_language: The language of the tokenizer to be constructed.
-            Various languages currently supported only in SpaCy.
-        pad_token (str): The string token used as padding. If ``nesting_field`` is
-            sequential, this will be set to its ``pad_token``. Default: ``"<pad>"``.
-        pad_first (bool): Do the padding of the sequence at the beginning. Default:
-            ``False``.
-    """
 
     def __init__(self, nesting_field, use_vocab=True, init_token=None, eos_token=None,
-                 fix_length=None, dtype=torch.long, preprocessing=None,
-                 postprocessing=None, tokenize=None, tokenizer_language='en',
+                 fix_length=None, tensor_type=torch.LongTensor, preprocessing=None,
+                 postprocessing=None, tokenize=lambda s: s.split(),
                  include_lengths=False, pad_token='<pad>',
-                 pad_first=False, truncate_first=False, vae_struct= False):
-        if isinstance(nesting_field, NestedField):
-            raise ValueError('nesting field must not be another NestedField')
+                 pad_first=False, truncate_first=False, vae_struct=False):
+  
         if nesting_field.include_lengths:
             raise ValueError('nesting field cannot have include_lengths=True')
 
         if nesting_field.sequential:
             pad_token = nesting_field.pad_token
-        super(NestedField, self).__init__(
+        super(SentenceWord_field, self).__init__(
             use_vocab=use_vocab,
             init_token=init_token,
             eos_token=eos_token,
             fix_length=fix_length,
-            dtype=dtype,
+            tensor_type=tensor_type,
             preprocessing=preprocessing,
             postprocessing=postprocessing,
             lower=nesting_field.lower,
             tokenize=tokenize,
-            tokenizer_language=tokenizer_language,
             batch_first=True,
             pad_token=pad_token,
             unk_token=nesting_field.unk_token,
@@ -110,7 +57,7 @@ class SentenceWord_field(Field):
             list: The preprocessed list.
         """
         return [self.nesting_field.preprocess(x)
-                for x in super(NestedField, self).preprocess(xs)]
+                for x in super(SentenceWord_field, self).preprocess(xs)]
 
     def pad(self, minibatch):
         """Pad a batch of examples using this field.
@@ -128,7 +75,7 @@ class SentenceWord_field(Field):
             >>> pp = pprint.PrettyPrinter(indent=4)
             >>>
             >>> nesting_field = Field(pad_token='<c>', init_token='<w>', eos_token='</w>')
-            >>> field = NestedField(nesting_field, init_token='<s>', eos_token='</s>')
+            >>> field = SentenceWord_field(nesting_field, init_token='<s>', eos_token='</s>')
             >>> minibatch = [
             ...     [list('john'), list('loves'), list('mary')],
             ...     [list('mary'), list('cries')],
@@ -156,7 +103,7 @@ class SentenceWord_field(Field):
         """
         minibatch = list(minibatch)
         if not self.nesting_field.sequential:
-            return super(NestedField, self).pad(minibatch)
+            return super(SentenceWord_field, self).pad(minibatch)
 
         # Save values of attributes to be monkeypatched
         old_pad_token = self.pad_token
@@ -180,7 +127,7 @@ class SentenceWord_field(Field):
         old_include_lengths = self.include_lengths
         self.include_lengths = True
         self.nesting_field.include_lengths = True
-        padded, sentence_lengths = super(NestedField, self).pad(minibatch)
+        padded, sentence_lengths = super(SentenceWord_field, self).pad(minibatch)
         padded_with_lengths = [self.nesting_field.pad(ex) for ex in padded]
         word_lengths = []
         final_padded = []
@@ -256,7 +203,7 @@ class SentenceWord_field(Field):
             kwargs["vectors_cache"] = None
         # just build vocab and does not load vector
         self.nesting_field.build_vocab(*flattened, **kwargs)
-        super(NestedField, self).build_vocab()
+        super(SentenceWord_field, self).build_vocab()
         self.vocab.extend(self.nesting_field.vocab)
         self.vocab.freqs = self.nesting_field.vocab.freqs.copy()
         if old_vectors is not None:
